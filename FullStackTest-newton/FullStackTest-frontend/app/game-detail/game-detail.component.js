3edb990be815213c067b1053e227ab83a789947c
Christian Reyes <creyes.prog@gmail.com>
2020-08-04 00:57:22 -0400

    Add form validation

--- FullStackTest-newton/FullStackTest-frontend/app/game-detail/game-detail.component.js
+++ FullStackTest-newton/FullStackTest-frontend/app/game-detail/game-detail.component.js
@@ -4,28 +4,30 @@ angular.
 module('gameDetail').
 component('gameDetail', {
   templateUrl: 'game-detail/game-detail.template.html',
-  controller: ['$state', '$stateParams', 'gameservice',
-    function GameDetailController($state, $stateParams, gameservice) {
+  controller: ['$state', '$stateParams', 'gameservice', '$scope',
+    function GameDetailController($state, $stateParams, gameservice, $scope) {
       var self = this;
 
       gameservice.getGame($stateParams.gameId).then(function (data) {
         self.game = data;
       });
 
-      self.submit = function submit() {
-        var gameDetailsObj = {
-          gameId: self.game.gameId,
-          title: self.game.title,
-          rating: self.game.rating,
-          developer: self.game.developer,
-          genre: self.game.genre,
-          description: self.game.description
-        };
+      $scope.submitForm = function (isValid) {
+        if (isValid) {
+          var gameDetailsObj = {
+            gameId: self.game.gameId,
+            title: self.game.title,
+            rating: self.game.rating,
+            developer: self.game.developer,
+            genre: self.game.genre,
+            description: self.game.description
+          };
 
-        gameservice.updateGame(gameDetailsObj).then(function (data) {
-          self.game = data;
-          $state.go('gameList');
-        });
+          gameservice.updateGame(gameDetailsObj).then(function (data) {
+            self.game = data;
+            $state.go('gameList');
+          });
+        }
       }
     }
   ]
